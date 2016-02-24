49f3acf3d3e4fce977d7394480e844fc39dea838
Chong Ri Yang <chongriyang@gmail.com>
2016-02-24 21:44:59 +0800

    Started on Task object -Included member variables

---
+++ Task.java
@@ -0,0 +1,18 @@
+import java.sql.Date;
+
+/**
+ * This class contains the attributes of each task in ArchPlanner
+ * @author riyang
+ *
+ */
+
+public class Task {
+	
+	//These are the data members of Task object
+	public static int _id;
+	public static boolean _status;
+	public static String _description;
+	public static String _tag;
+	public static Date _start_time;
+	public static Date _end_time;
+}
