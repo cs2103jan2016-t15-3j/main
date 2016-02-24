50029c2e3ef02f3f307c402de38e3a418843ab55
Chong Ri Yang <chongriyang@gmail.com>
2016-02-24 22:24:32 +0800

    Added setter to Task Object -Minor changes: removed variable _id Renamed variable _status to _isDone Added new variable boolean _isOverDue

--- ArchPlanner/src/Task.java
+++ ArchPlanner/src/Task.java
@@ -9,11 +9,39 @@ import java.sql.Date;
 public class Task {
 	
 	//These are the data members of Task object
-	public static int _id;
-	public static boolean _status;
+	public static String _name;
 	public static String _description;
 	public static String _tag;
-	public static Date _start_time;
-	public static Date _end_time;
+	public static Date _startTime;
+	public static Date _endTime;
+	public static boolean _isDone;
+	public static boolean _isOverDue;
 	
+	public static void setName(String name) {
+		_name = name;
+	}
+	
+	public static void setDescription(String description) {
+		_description = description;
+	}
+	
+	public static void setTag(String tag) {
+		_tag = tag;
+	}
+	
+	public static void setStartTime(Date startTime) {
+		_startTime = startTime;
+	}
+	
+	public static void setEndTime(Date endTime) {
+		_endTime = endTime;
+	}
+	
+	public static void setIsDone(boolean isDone) {
+		_isDone = isDone;
+	}
+	
+	public static void setIsOverDue(boolean isOverDue) {
+		_isOverDue = isOverDue;
+	}
 }
