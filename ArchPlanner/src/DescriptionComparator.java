4afc4c54f2492cedb124c5a3f2b206b3fab14a6e
Chong Ri Yang <chongriyang@gmail.com>
2016-02-25 23:01:52 +0800

    Added Description comparator

---
+++ ArchPlanner/src/DescriptionComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class DescriptionComparator implements Comparator<Task> {
+	
+	public int compare(Task task1, Task task2) {
+		return task1.getDescription().compareTo(task2.getDescription());
+	}
+	
+	public boolean equals(Object obj) {
+		return this == obj;
+	}
+}
