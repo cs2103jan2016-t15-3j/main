b8651fd2b8aeb9df5aae3bb1711d6884092cd245
Chong Ri Yang <chongriyang@gmail.com>
2016-02-25 23:02:44 +0800

    Added end time comparator for sorting

---
+++ ArchPlanner/src/EndTimeComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class EndTimeComparator implements Comparator<Task> {
+	
+	public int compare(Task task1, Task task2) {
+		return task1.getEndTime().compareTo(task2.getEndTime());
+	}
+	
+	public boolean equals(Object obj) {
+		return this == obj;
+	}
+}
