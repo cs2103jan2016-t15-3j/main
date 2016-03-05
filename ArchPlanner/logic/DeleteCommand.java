8f4e1ab9ecfdfef834f608ebea336192fda66f0d
Chong Ri Yang <chongriyang@gmail.com>
2016-03-05 21:56:54 +0800

    Edited Constructor

--- ArchPlanner/logic/DeleteCommand.java
+++ ArchPlanner/logic/DeleteCommand.java
@@ -4,6 +4,10 @@ public class DeleteCommand implements Command {
 	
 	private int _id;
 	
+	public DeleteCommand(int id) {
+		_id = id;
+	}
+	
 	public void setId(int id) {
 		assert(id >= 1); //id can only be 1 or greater
 		_id = id;
