5c4f7d1b00e52867f21b9ff0fdd8d57e62f38a5c
riyang <chongriyang@gmail.com>
2016-03-15 19:06:51 +0800

    debug ListsManager object

--- ArchPlanner/logic/ListsManager.java
+++ ArchPlanner/logic/ListsManager.java
@@ -21,7 +21,7 @@ public class ListsManager {
 
 		clearAllLists();
 		_mainList.addAll(list);
-		
+
 		SortMechanism sort = new SortMechanism();
 		sort.sortListByDescription(_mainList);
 		_mainList = sort.sortListByDateTime(_mainList);
@@ -37,28 +37,30 @@ public class ListsManager {
 			} else if ((task.getStartDateTime() != null) && (task.getEndDateTime() != null)) {
 				_deadlineList.add(task);
 			}
-			
+
 			if (task.getIsDone() == true) {
 				_doneList.add(task);
 			} else {
 				_undoneList.add(task);
 			}
-			
-			String tag = task.getTag();
-			if ((tag != null) && (!_tagsList.contains(tag))) {
-				_tagsList.add(tag);
+
+			for (int j = 0; j < task.getTagsList().size() ; j++) {
+				String tag = task.getTagsList().get(j);
+				if ((tag != null) && (!_tagsList.contains(tag))) {
+					_tagsList.add(tag);
+				}
 			}
 		}
 		/*
 		for(int i = 0; i < _undoneList.size(); i++)
 			System.out.println("main" + i + "\t" + _mainList.get(i).getDescription());
-		
+
 		for(int i = 0; i < _undoneList.size(); i++)
 			System.out.println("undone" + i + "\t" + _undoneList.get(i).getDescription());
-		*/
+		 */
 		sort.sortTagsList(_tagsList);
 		setViewList(_viewType);
-		
+
 		_tagsList.add(0, "Deadlines");
 		_tagsList.add(1 ,"Events");
 		_tagsList.add(2, "Tasks");
@@ -74,13 +76,13 @@ public class ListsManager {
 		_doneList.clear();
 		_undoneList.clear();
 	}
-	
+
 	public void setViewList(String viewType) {
 		_viewType = viewType;
 		_viewList.clear();
 
 		System.out.println(_viewType);
-		
+
 		if (_viewType.equalsIgnoreCase("VIEW_ALL")) {
 			_viewList.addAll(_undoneList);
 		} else if (_viewType.equals("VIEW_DONE")) {
@@ -89,20 +91,20 @@ public class ListsManager {
 			_viewList.addAll(_searchResultList);
 		}
 	}
-	
+
 	public void setSearchList(ArrayList<Task> searchResultList) {
 		_searchResultList.clear();
 		_searchResultList.addAll(searchResultList);
 	}
-	
+
 	public String getViewType() {
 		return _viewType;
 	}
-	
+
 	public ArrayList<Task> getMainList() {
 		return _mainList;
 	}
-	
+
 	public ArrayList<Task> getUndoneList() {
 		return _undoneList;
 	}
@@ -110,11 +112,11 @@ public class ListsManager {
 	public ArrayList<Task> getViewList() {
 		return _viewList;
 	}
-	
+
 	public ArrayList<String> getTagsList() {
 		return _tagsList;
 	}
-	
+
 	public ArrayList<Task> getSearchResultList() {
 		return _searchResultList;
 	}
