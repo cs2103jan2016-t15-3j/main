d8784f72ac73768d6aedd8c63ef1c12e0601c9af
riyang <chongriyang@gmail.com>
2016-03-23 23:57:55 +0800

    upated with new comparators for done, overdue and tag.

---
+++ ArchPlanner/comparator/DoneComparator.java
@@ -0,0 +1,17 @@
+package comparator;
+import java.util.Comparator;
+
+import logic.Task;
+
+public class DoneComparator implements Comparator<Task> {
+
+	public int compare(Task task1, Task task2) {
+		if (task1.getIsDone() == true && task2.getIsDone() == false){
+			return 1;
+		} else if (task1.getIsDone() == false && task2.getIsDone() == true){
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+}
